eba715d8cef7c44a9b2db33089e118441ce814d8
Ezequiel Calderara <ezechico@gmail.com>
2015-03-17 17:44:03 -0300

    Initial Commit

---
+++ Assets/Scripts/TiledLevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiledLevelGenerator : MonoBehaviour {
+
+	public TiledFileInfo FileInfo;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Generate()
+    {
+
+        for (int i = 0; i < FileInfo.TiledFile.Map.Layers.Length; i++)
+        {
+            var layer = FileInfo.TiledFile.Map.Layers[i];
+            for (int j = 0; j < layer.data.tiles.Length; j++)
+            {
+
+            }
+
+        }
+    }
+}
